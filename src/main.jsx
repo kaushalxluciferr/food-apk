ed4f8d276dc212d3c1b0e1201768fa5dd1df7051
Kaushal Kishor Rauniyar <22054382@kiit.ac.in>
2024-10-23 17:46:07 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import Storecontextprovider from './context/Storecontext.jsx'
-import Home from '../../ka/src/Home.jsx'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Storecontextprovider>
